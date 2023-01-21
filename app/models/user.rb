0e446deda763a50aa407bc6a38b36f0fd11c3218
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-21 17:31:24 +0900

    added has_many to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  has_many :services, dependent: :destroy
+  has_many :bookings
+  has_many :bookings_as_cleaner, through: :services, source: :bookings
 end
