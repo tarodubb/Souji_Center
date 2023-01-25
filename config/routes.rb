99ab3913444215d3541ad60ed52b0fcb7d127f97
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-25 10:32:37 +0900

    adding links to the navbar, creating bookings controller for new, create and destroy actions, creating view for new bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
   resources :services do
     resources :bookings, only: [:new, :create]
   end
