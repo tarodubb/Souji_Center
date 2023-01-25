0b53449c72e037921c29b3f1cf49023b524b3cf7
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-25 20:29:07 +0900

    you can now see the services index and show pages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,12 +2,15 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @service = List.find(params[:service_id])
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
     @service = Service.find(params[:service_id])
     @booking.service = @service
+    @booking.user = current_user
+    authorize @booking
     @booking.save
     if @booking.save
       redirect_to booking_path(@service)
@@ -20,6 +23,8 @@ class BookingsController < ApplicationController
     @service = Service.find(params[:service_id])
     @booking = Booking.find(params[:id])
     @booking.service = @service
+    @booking.user = current_user
+    authorize @booking
     @booking.destroy
     redirect_to service_path(@service), status: :see_other
   end
@@ -30,3 +35,14 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:comment, :list_id, :movie_id)
   end
 end
+``
+# def new
+#   @restaurant = Restaurant.new
+#   authorize @restaurant # Add this line
+# end
+# # [...]
+# def create
+#   @restaurant = Restaurant.new(restaurant_params)
+#   @restaurant.user = current_user
+#   authorize @restaurant # Add this line
+#   # [...]
