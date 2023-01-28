cf976d050a6947682fb3d7747c97225efff829e2
radektyszkiewicz <ebisu.poland@gmail.com>
2023-01-29 08:54:37 +0900

    Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :services, dependent: :destroy
   has_many :bookings
   has_many :bookings_as_cleaner, through: :services, source: :bookings
-  has_many_attached :photos
+  has_one_attached :photo
 end
