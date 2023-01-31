e2dbc1b0259ff7cc5a50cc1bf1e235131419d51b
Natalia Chernenko <natchernenko13@gmail.com>
2023-01-31 17:52:52 +0900

    cleaner bookings page

--- app/controllers/cleaner/bookings_controller.rb
+++ app/controllers/cleaner/bookings_controller.rb
@@ -6,12 +6,12 @@ class Cleaner::BookingsController < ApplicationController
 
   def approve
     @booking = Booking.find(params[:id])
-    @booking.update(state: "approved")
-    if @booking.state == "approved"
-    flash[:success] = "Booking successfully approved"
-    redirect_to bookings_path
+    @booking.update(status: 1)
+    if @booking.status == 1
+      flash[:success] = "Booking successfully approved"
+    redirect_to booking_path(@booking)
     else
-    flash[:error] = "Booking not approved"
+      flash[:error] = "Booking not approved"
     redirect_to bookings_path
     end
   end
